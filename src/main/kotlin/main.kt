ac4620bc544595eca57a3855cb560ad14b845eee
Rita R <prostorita2013.p@yandex.ru>
2023-03-09 22:03:18 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val result = calculateCommission( purchaseAmount = 70000)
+    println(result)
+}
+
+fun calculateCommission(cardType: String = "VK Pay", purchaseAmount: Int, purchaseAmountMonth: Int = 0): Int? {
+    val commissionRate = when (cardType) {
+        "mastercard", "maestro" -> if (purchaseAmount >= 75000) (0.06 * purchaseAmount) + 20 else 0
+        "visa", "mir" -> if ((0.075 * purchaseAmount) < 35) 35 else purchaseAmount * 0.075
+        else -> 0
+    }
+    return when {
+        purchaseAmount > dailyLimit(cardType) || (purchaseAmountMonth + purchaseAmount) > monthlyLimit(cardType) -> null
+        purchaseAmountMonth > monthlyLimit(cardType) -> null
+        else -> commissionRate.toInt()
+    }
+}
+fun dailyLimit(cardType: String) = when (cardType) {
+    "VK Pay" -> 15000
+    else -> 150000
+}
+fun monthlyLimit(cardType: String) = when (cardType) {
+    "VK Pay" -> 40000
+    else -> 600000
+}
+
