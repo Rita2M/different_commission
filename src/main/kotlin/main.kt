d36fcf7b3ac9269bc2dcb1bca68da0a1c936df38
Rita R <prostorita2013.p@yandex.ru>
2023-04-03 14:55:04 +0300

    autotest add

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,17 +1,15 @@
-fun main() {
-    val result = calculateCommission( purchaseAmount = 70000)
-    println(result)
+fun main(){
+     calculateCommission(purchaseAmount = 70000)
 }
 
 fun calculateCommission(cardType: String = "VK Pay", purchaseAmount: Int, purchaseAmountMonth: Int = 0): Int? {
     val commissionRate = when (cardType) {
         "mastercard", "maestro" -> if (purchaseAmount >= 75000) (0.06 * purchaseAmount) + 20 else 0
-        "visa", "mir" -> if ((0.075 * purchaseAmount) < 35) 35 else purchaseAmount * 0.075
+         "visa", "mir" -> if ((0.075 * purchaseAmount) < 35) 35 else purchaseAmount * 0.075
         else -> 0
     }
     return when {
         purchaseAmount > dailyLimit(cardType) || (purchaseAmountMonth + purchaseAmount) > monthlyLimit(cardType) -> null
-        purchaseAmountMonth > monthlyLimit(cardType) -> null
         else -> commissionRate.toInt()
     }
 }
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,45 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun calculateCommissionMir(){
+        val result = calculateCommission( "mir", 100, 650000)
+
+        assertEquals(null, result)
+    }
+    @Test
+    fun calculateCommissionMaestro(){
+        val result = calculateCommission("maestro",76000)
+        assertEquals(4580, result)
+    }
+    @Test
+    fun calculateCommissionVk(){
+        val result = calculateCommission( purchaseAmount = 5000)
+        assertEquals(0, result)
+    }
+    @Test
+    fun calculateCommissionVisa(){
+        val result = calculateCommission("visa",10000)
+        assertEquals(750, result)
+    }
+    @Test
+    fun calculateCommissionMastercard(){
+        val result = calculateCommission("mastercard",10000)
+
+        assertEquals(0, result)
+
+    }
+    @Test
+    fun calculateCommissionLimit(){
+        val result = calculateCommission(purchaseAmount = 20000)
+        assertEquals(null, result)
+    }
+    @Test
+    fun calcMain() {
+        val result = main()
+        assertEquals(Unit, result)
+    }
+}
